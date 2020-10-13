090b3c31a73f493463a326ecba425a9be6ace726
reetou <fillmasterson@gmail.com>
2020-10-13 17:20:23 +0300

    Show log on establish error

--- lib/phoenix_pubsub_redis/redis_server.ex
+++ lib/phoenix_pubsub_redis/redis_server.ex
@@ -126,7 +126,9 @@ defmodule Phoenix.PubSub.RedisServer do
     redis_opts = Keyword.take(state.opts, @redix_opts)
     case Redix.PubSub.start_link(Keyword.merge(redis_opts, sync_connect: true)) do
       {:ok, redix_pid} -> establish_success(%{state | redix_pid: redix_pid})
-      {:error, _} ->
+      {:error, _} = error ->
+        IO.inspect(error, label: "Error establishing connection")
+        Logger.error("Pubsub: Cannot establish connection: #{inspect error}")
         establish_failed(state)
     end
   end
